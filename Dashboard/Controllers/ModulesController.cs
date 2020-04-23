d8b471ea3e9b5d6ebe881738fb6e3eb90bc00957
fortress <david.la.gordt.dillie@gmail.com>
2020-04-23 21:31:03 +0200

    Add project files.

---
+++ Dashboard/Controllers/ModulesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class ModulesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Moderation()
+        {
+            return View();
+        }
+    }
+}
